ea65fccc7ef773020a981aeff4dfa7e018814ffc
Praveen-Hapuarachchi <hapup14@gmail.com>
2025-03-24 11:04:21 +0530

    Update

--- simple-blog/src/api-helper.js
+++ simple-blog/src/api-helper.js
@@ -1,4 +1,5 @@
-export const base_url = 'http://localhost:5000';
+//export const base_url = 'http://localhost:5000';
+export const base_url = 'http://68.183.83.217:5000';
 
 export const signupUser = async (userData) => {
     try {
