222d75c35200024e3a3223aba4dfb1d087afec2b
Praveen-Hapuarachchi <hapup14@gmail.com>
2025-03-28 10:12:51 +0530

    Update

--- simple-blog/src/api-helper.js
+++ simple-blog/src/api-helper.js
@@ -2,26 +2,8 @@
 // export const base_url = 'http://68.183.83.217'; // Use port 80 (nginx)
 
 export const signupUser = async (userData) => {
-    try {
-      const response = await fetch(`${base_url}/api/auth/signup`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-      });
-
-      const result = await response.json();
-      return { result, status: response.ok };
-    } catch (error) {
-      console.error('Error signing up:', error);
-      return { result: 'There was an error signing up. Please try again later.', status: false };
-    }
-};
-
-export const loginUser = async (userData) => {
   try {
-    const response = await fetch(`${base_url}/api/auth/login`, {
+    const response = await fetch('/api/auth/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -32,7 +14,25 @@ export const loginUser = async (userData) => {
     const result = await response.json();
     return { result, status: response.ok };
   } catch (error) {
-    console.error('Error logging in:', error);
-    return { result: 'There was an error logging in. Please try again later.', status: false };
+    console.error('Error signing up:', error);
+    return { result: 'There was an error signing up. Please try again later.', status: false };
   }
+};
+
+export const loginUser = async (userData) => {
+try {
+  const response = await fetch('/api/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+
+  const result = await response.json();
+  return { result, status: response.ok };
+} catch (error) {
+  console.error('Error logging in:', error);
+  return { result: 'There was an error logging in. Please try again later.', status: false };
+}
 };
