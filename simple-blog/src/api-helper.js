6a67490aebab46558973400a10cd38feb429dd1e
Praveen-Hapuarachchi <hapup14@gmail.com>
2025-03-28 15:10:10 +0530

    Update

--- simple-blog/src/api-helper.js
+++ simple-blog/src/api-helper.js
@@ -1,5 +1,5 @@
 // export const base_url = 'http://localhost:5000';
-export const base_url = 'http://68.183.83.217:5000'; // Backend port
+export const base_url = 'http://68.183.83.217:5000';
 
 export const signupUser = async (userData) => {
   try {
@@ -10,7 +10,6 @@ export const signupUser = async (userData) => {
       },
       body: JSON.stringify(userData),
     });
-
     const result = await response.json();
     return { result, status: response.ok };
   } catch (error) {
@@ -28,7 +27,6 @@ export const loginUser = async (userData) => {
       },
       body: JSON.stringify(userData),
     });
-
     const result = await response.json();
     return { result, status: response.ok };
   } catch (error) {
