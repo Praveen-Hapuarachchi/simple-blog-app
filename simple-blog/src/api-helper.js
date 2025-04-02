08b538989037a8c1b0c367e1c1a3db10eb44e12a
Praveen-Hapuarachchi <hapup14@gmail.com>
2025-04-02 12:46:41 +0530

    update

--- simple-blog/src/api-helper.js
+++ simple-blog/src/api-helper.js
@@ -1,6 +1,6 @@
 // export const base_url = 'http://localhost:5000/api';
-// export const base_url = 'http://159.89.175.60/api';
-export const base_url = 'http://209.38.173.211/api'; // Relative path, proxied by Nginx
+export const base_url = 'http://159.89.175.60/api';
+// export const base_url = 'http://209.38.173.211/api'; // Relative path, proxied by Nginx
 
 export const signupUser = async (userData) => {
   try {
